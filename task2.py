49936cdc70c1cee4ad65ca73894502ecc2dfcdb8
CyberShur <102461318+CyberShur@users.noreply.github.com>
2022-07-16 15:49:30 +0300

    рещение task2

--- task2.py
+++ task2.py
@@ -3,3 +3,18 @@
 Пример:
 - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 '''
+
+N = int(input('Введите число N: '))
+
+def line(a):
+    rez = []
+    for i in range(1, a+1):
+        if i in [1]:
+            rez.append(i)
+        else:
+            rez.append(rez[-1]*i)
+    return rez
+
+res = line(N)
+
+print(res)
